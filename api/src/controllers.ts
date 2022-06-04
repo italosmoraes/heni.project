ec12c9659a3b38a7a4aed85a5b5cb5685bf7add0
Italo <italoe@gmail.com>
2022-06-04 16:09:44 +0100

    ...

--- api/src/controllers.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from 'express'
-
-const PRINTS_API = 'https://api.harvardartmuseums.org'
-
-export const PrintsHandler = (req: Request, res: Response) => {
-  res.send('PrintsHandler...')
-}
