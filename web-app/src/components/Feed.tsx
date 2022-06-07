e6c97ede0475090f7c9ae9ff7cc7e73b8d11f07e
Italo <italoe@gmail.com>
2022-06-07 15:27:31 +0100

    a few adjustments and organizing

--- web-app/src/components/Feed.tsx
+++ web-app/src/components/Feed.tsx
@@ -1,28 +1,9 @@
-import { Print } from '../types/Print'
 import styled from 'styled-components'
 import { useQuery } from '@apollo/client'
 import { PrintsResponse } from '../graphql/types'
 import { PRINTS_QUERY } from '../graphql/queries'
 import { useEffect, useState } from 'react'
-
-type FeedItemProps = {
-  item: Print
-}
-
-const FeedItemRow = styled.p`
-  font-size: 0.75em;
-  flex: 1;
-`
-
-const FeedItemContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-content: center;
-  padding: 25px;
-  border: 2px solid palevioletred;
-  border-radius: 3px;
-`
+import { FeedItem } from './FeedItem'
 
 const FeedContainer = styled.div`
   display: flex;
@@ -36,28 +17,13 @@ const LoadMoreBtn = styled.button`
   margin: 10px;
 `
 
-export const FeedItem = (props: FeedItemProps) => {
-  const { item } = props
-  console.log('>>> img', item.primaryImageUrl)
-  return (
-    <FeedItemContainer>
-      <img src={item.primaryImageUrl} width={400} height={600} />
-      <FeedItemRow>Title: {item.title}</FeedItemRow>
-      <FeedItemRow>Description: {item.description}</FeedItemRow>
-      <FeedItemRow>Department: {item.dated}</FeedItemRow>
-      <a href={item.url} target="_blank">
-        View
-      </a>
-    </FeedItemContainer>
-  )
-}
-
+const LOADING_LIMIT = 10
 export const Feed = () => {
   const { loading, error, data, refetch } = useQuery<PrintsResponse>(PRINTS_QUERY, {
     variables: {
       input: {
         resource: 'OBJECT', // TODO remove since we are only querying OBJECTs in the api
-        limit: 1,
+        limit: LOADING_LIMIT,
         page: 1
       }
     }
@@ -66,14 +32,11 @@ export const Feed = () => {
   const [feedItems, setFeedItems] = useState<any[]>([]) // TODO improve type for feed items
 
   useEffect(() => {
-    console.log('>>>> data', data)
-
     if (data?.prints && nextPage !== data?.prints.info.pages) {
       setNextPage(data.prints.info.page + 1)
     }
 
     if (data?.prints.records) {
-      console.log('>>> set Fee items', data?.prints.records)
       setFeedItems([...feedItems, ...data.prints.records])
     }
   }, [data])
@@ -82,7 +45,7 @@ export const Feed = () => {
     refetch({
       input: {
         resource: 'OBJECT', // TODO remove since we are only querying OBJECTs in the api
-        limit: 1,
+        limit: LOADING_LIMIT,
         page: nextPage
       }
     })
---
+++ web-app/src/components/FeedItem.tsx
@@ -0,0 +1,41 @@
+import styled from 'styled-components'
+import { Print } from '../types/Print'
+
+type FeedItemProps = {
+  item: Print
+}
+
+const FeedItemRow = styled.p`
+  font-size: 0.75em;
+  flex: 1;
+`
+
+const FeedItemContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+  padding: 25px;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+  max-width: 75%;
+`
+
+export const FeedItem = (props: FeedItemProps) => {
+  const { item } = props
+
+  return (
+    <FeedItemContainer>
+      {item.primaryImageUrl && <img src={item.primaryImageUrl} width={400} height={600} />}
+      {item.title && <FeedItemRow>Title: {item.title}</FeedItemRow>}
+      {item.description && <FeedItemRow>Description: {item.description}</FeedItemRow>}
+      {item.department && <FeedItemRow>Department: {item.department}</FeedItemRow>}
+      {item.dated && <FeedItemRow>Dated: {item.dated}</FeedItemRow>}
+      {item.url && (
+        <a href={item.url} target="_blank">
+          View at Harvard Museum
+        </a>
+      )}
+    </FeedItemContainer>
+  )
+}
