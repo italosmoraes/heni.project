f996c16359157f0414788629b140781ef299df97
Italo <italoe@gmail.com>
2022-06-07 15:07:18 +0100

    fetch api with pagination

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,14 +1,23 @@
+import styled from 'styled-components'
 import './App.css'
 import { Feed } from './components/Feed'
+
+const PageContainer = styled.div`
+  display: flex;
+  margin-bottom: 50px;
+  align-items: center;
+  justify-content: center;
+`
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h3>Prints Feed</h3>
-        <hr style={{ width: '50%' }} />
-        <Feed />
       </header>
+      <PageContainer>
+        <Feed />
+      </PageContainer>
     </div>
   )
 }
--- web-app/src/components/Feed.tsx
+++ web-app/src/components/Feed.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 import { useQuery } from '@apollo/client'
 import { PrintsResponse } from '../graphql/types'
 import { PRINTS_QUERY } from '../graphql/queries'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 type FeedItemProps = {
   item: Print
@@ -19,15 +19,29 @@ const FeedItemContainer = styled.div`
   flex-direction: column;
   justify-content: center;
   align-content: center;
-  padding: 0;
+  padding: 25px;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`
+
+const FeedContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+  padding: 10px;
+`
+
+const LoadMoreBtn = styled.button`
+  margin: 10px;
 `
 
 export const FeedItem = (props: FeedItemProps) => {
   const { item } = props
-
+  console.log('>>> img', item.primaryImageUrl)
   return (
     <FeedItemContainer>
-      <img src={item.primaryImageUrl} width={'400em'} height={'600em'} />
+      <img src={item.primaryImageUrl} width={400} height={600} />
       <FeedItemRow>Title: {item.title}</FeedItemRow>
       <FeedItemRow>Description: {item.description}</FeedItemRow>
       <FeedItemRow>Department: {item.dated}</FeedItemRow>
@@ -39,19 +53,40 @@ export const FeedItem = (props: FeedItemProps) => {
 }
 
 export const Feed = () => {
-  const { loading, error, data } = useQuery<PrintsResponse>(PRINTS_QUERY, {
+  const { loading, error, data, refetch } = useQuery<PrintsResponse>(PRINTS_QUERY, {
     variables: {
       input: {
         resource: 'OBJECT', // TODO remove since we are only querying OBJECTs in the api
         limit: 1,
-        page: null
+        page: 1
       }
     }
   })
+  const [nextPage, setNextPage] = useState(1)
+  const [feedItems, setFeedItems] = useState<any[]>([]) // TODO improve type for feed items
 
   useEffect(() => {
-    console.log('>>>>', data)
+    console.log('>>>> data', data)
+
+    if (data?.prints && nextPage !== data?.prints.info.pages) {
+      setNextPage(data.prints.info.page + 1)
+    }
+
+    if (data?.prints.records) {
+      console.log('>>> set Fee items', data?.prints.records)
+      setFeedItems([...feedItems, ...data.prints.records])
+    }
   }, [data])
+
+  const loadMore = () => {
+    refetch({
+      input: {
+        resource: 'OBJECT', // TODO remove since we are only querying OBJECTs in the api
+        limit: 1,
+        page: nextPage
+      }
+    })
+  }
 
   if (loading) return <p>Loading...</p>
 
@@ -62,12 +97,12 @@ export const Feed = () => {
         {error}
       </>
     )
-
   return (
-    <>
-      {data &&
-        data.prints &&
-        data.prints.records.map((item) => <FeedItem key={item} item={item} />)}
-    </>
+    <FeedContainer>
+      {feedItems && feedItems.map((item, idx) => <FeedItem key={idx} item={item} />)}
+      <LoadMoreBtn name="load-more" onClick={() => loadMore()}>
+        ...LOAD MORE
+      </LoadMoreBtn>
+    </FeedContainer>
   )
 }
